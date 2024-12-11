2f17e5c9a2b5d696dbe0307094f7d6956fa2fce5
luboshellesch <lubos.hellesch@gmail.com>
2024-12-11 17:15:13 +0100

    second commit

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -12,4 +12,19 @@ public class Tests
     {
         Assert.Pass();
     }
+    [Test]
+    public void Test2()
+    {
+        Assert.Pass();
+    }
+    [Test]
+    public void Test3()
+    {
+        Assert.Pass();
+    }
+    [Test]
+    public void Test4()
+    {
+        Assert.Pass();
+    }
 }
